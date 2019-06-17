2fbb95165fdbe7c52c2e6fb1f49c3314d97d6df0
Andrew Bateman <gomezbateman@yahoo.com>
2019-06-17 22:41:04 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+
+import { AuthComponent } from './auth/auth.component';
+import { TodoComponent } from './todo/todo.component';
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		AuthComponent,
+		TodoComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		AmplifyAngularModule
+	],
+	providers: [AmplifyService],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
